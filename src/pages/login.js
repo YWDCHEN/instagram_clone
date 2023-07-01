0a076c23406e0cacb5ff635feb42f3cd2c506794
YWDCHEN <ywdchen@uwaterloo.ca>
2023-07-01 18:07:57 -0400

    signin setup

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,22 @@
 import {useNavigate} from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import FirebaseContext from "../context/firebase";
 
 function Login() {
   const history = useNavigate();
   const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAdress] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+  const isValid = password == "" || emailAddress == "";
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = 'Login - Instagram';
+  }, []);
 
   return <p> login page ! </p>;
 }
