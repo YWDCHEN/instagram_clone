816c810b272adefbd548b385857a818781a07e60
YWDCHEN <ywdchen@uwaterloo.ca>
2023-07-02 14:42:33 -0400

    tailwind setup

--- src/pages/login.js
+++ src/pages/login.js
@@ -18,7 +18,16 @@ function Login() {
     document.title = 'Login - Instagram';
   }, []);
 
-  return <p> login page ! </p>;
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpg" alt="iPhone with Ins app" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <p>i will be form! </p>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
