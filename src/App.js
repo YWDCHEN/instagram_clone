5d082f0af4e4ff44bd351e23b9b5faf064e6d7af
YWDCHEN <ywdchen@uwaterloo.ca>
2023-07-03 23:40:17 -0400

    sign up page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { Suspense } from "react";
-import  {BrowserRouter, Route,Routes} from "react-router-dom";
-import { lazy } from "react";
-import * as ROUTES from "./constants/routes";
+import React, {Suspense} from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {lazy} from 'react';
+import * as ROUTES from './constants/routes';
 
-const Login = lazy(() => import("./pages/login"));
+const Login = lazy(() => import('./pages/login'));
+const Signup = lazy(() => import('./pages/signup'));
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Suspense fallback={<p>Loading...</p>}>
         <Routes>
           <Route path="/login" Component={Login} />
+          <Route path="/signup" Component={Signup} />
         </Routes>
       </Suspense>
     </BrowserRouter>
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,8 +2,9 @@ import {Link, useNavigate} from 'react-router-dom';
 import {useContext, useState, useEffect} from 'react';
 import FirebaseContext from '../context/firebase';
 import * as ROUTES from '../constants/routes';
+import {doesUsernameExist} from '../services/firebase';
 
-function Login() {
+function Signup() {
   const history = useNavigate();
   const {firebase} = useContext(FirebaseContext);
 
@@ -19,18 +20,40 @@ function Login() {
   const handleLogin = async e => {
     e.preventDefault();
 
-    try {
-      await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
-      history.push(ROUTES.DASHBOARD);
-    } catch (error) {
-      setEmailAdress('');
-      setPassword('');
-      setError(error.message);
+    const usernameExist = await doesUsernameExist(username);
+
+    if (!usernameExist) {
+      try {
+        const createdUserResult = await firebase
+          .auth()
+          .createUserWithEmailAndPassword(emailAddress, password);
+        await createdUserResult.user.updateProfile({
+          displayName: username,
+        });
+
+        await firebase.firestore().collection('user').add({
+          userId: createdUserResult.user.uid,
+          username: username.toLowerCase(),
+          fullname,
+          emailAddress: emailAddress.toLowerCase(),
+          following: [],
+          dateCreated: Date.now(),
+        });
+
+        history.push(ROUTES.DASHBOARD);
+      } catch (error) {
+        setFullname('');
+        setEmailAdress('');
+        setPassword('');
+        setError(error.message);
+      }
+    }else{
+      setError('This username is already taken, please try another.');
     }
   };
 
   useEffect(() => {
-    document.title = 'Login - Instagram';
+    document.title = 'Sign Up - Instagram';
   }, []);
 
   return (
@@ -50,6 +73,22 @@ function Login() {
           {error && <p className="mb-4 text-xs text-red-primary"> {error} </p>}
 
           <form onSubmit={handleLogin} method="POST">
+          <input
+              aria-label="Enter your username"
+              type="text"
+              placeholder="Username"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-4"
+              onChange={e => setUsername(e.target.value)}
+              value={username}
+            />
+            <input
+              aria-label="Enter your full name"
+              type="text"
+              placeholder="Full name"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-4"
+              onChange={e => setFullname(e.target.value)}
+              value={fullname}
+            />
             <input
               aria-label="Enter your email address"
               type="text"
@@ -73,14 +112,14 @@ function Login() {
                 isInvalid && 'opacity-50'
               }`}
             >
-              Log In
+              Sign Up
             </button>
           </form>
         </div>
         <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
-          <p className="text-sm"> Don't have an account?{' '}
-            <Link to="/signup" className="font-bold text-blue-medium">
-            Sign up
+          <p className="text-sm"> Have an account?{' '}
+            <Link to="/login" className="font-bold text-blue-medium">
+            Log In
             </Link>
           </p>
         </div>
@@ -89,4 +128,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Signup;
