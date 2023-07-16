a50fb733334b4d06332ea29d2da35c7a2c820ae6
YWDCHEN <ywdchen@uwaterloo.ca>
2023-07-16 17:16:38 -0400

    suggested profile

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,4 +1,5 @@
-import {firebase,FieldValue} from '../lib/firebase';
+// eslint-disable-next-line no-unused-vars
+import {firebase, FieldValue} from '../lib/firebase';
 
 export async function doesUsernameExist(username) {
   const result = await firebase
@@ -13,7 +14,7 @@ export async function doesUsernameExist(username) {
 }
 
 //get user from the firestore based on the userId passed by the auth
-export async function getUserByUserId(userId){
+export async function getUserByUserId(userId) {
   const result = await firebase
     .firestore()
     .collection('users')
@@ -25,4 +26,49 @@ export async function getUserByUserId(userId){
     docId: item.id,
   }));
   return user;
+}
+
+export async function getSuggestedProfiles(userId, following = []) {
+  const result = await firebase.firestore().collection('users').limit(10).get();
+
+  return result.docs
+    .map(user => ({...user.data(), docId: user.id}))
+    .filter(
+      (profile) =>
+        profile.userId !== userId && !following.includes(profile.userId),
+    );
+}
+
+//updateLoggedInUserFollowing, updateFollowedUserFollowers
+
+export async function updateLoggedInUserFollowing(
+  loggedInUserDocId,
+  profileId,
+  isFollowing,
+) {
+  return firebase
+    .firestore()
+    .collection('users')
+    .doc(loggedInUserDocId)
+    .update({
+      following: isFollowing
+        ? FieldValue.arrayRemove(profileId)
+        : FieldValue.arrayUnion(profileId),
+    });
+}
+
+export async function updateFollowedUserFollowers(
+  profileDocId,
+  loggedInUserDocId,
+  isFollowing,
+) {
+  return firebase
+    .firestore()
+    .collection('users')
+    .doc(profileDocId)
+    .update({
+      following: isFollowing
+        ? FieldValue.arrayRemove(loggedInUserDocId)
+        : FieldValue.arrayUnion(loggedInUserDocId),
+    });
 }
