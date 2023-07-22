990414634b6433c7babc85ec3a04d79861c93934
YWDCHEN <ywdchen@uwaterloo.ca>
2023-07-22 01:26:48 -0400

    use-photos hooks

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,4 +1,13 @@
+import Skeleton from "react-loading-skeleton";
+import usePhotos from '../hooks/use-photos';
+
 export default function Timeline() {
+  //get the logged in user's photos
+  const {photos} = usePhotos();
+  console.log('photos',photos);
+  //loading the photos using react skeleton
+  //render photos
+  //if no photos, suggest user to post photos
   return (
     <div className="container col-span-2">
       <p>timeline </p>
